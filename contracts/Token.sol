18f7cc02710d427ed39e08eba625407af2e5d4d4
Max <dev@Open.so>
2020-12-23 09:35:45 +0530

    .gitignore fixed

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -106,7 +106,7 @@ contract Token{
    }
 
    modifier onlyAdmin()	{
-	   require(msg.sender == admin, "No permissions");
+	   require(msg.sender == admin, "Only an admin can make this call");
 	   _;
    }
 
