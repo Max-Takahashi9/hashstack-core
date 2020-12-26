abcb7fa96c7e1c4a9176ae853b308d0b177d5ec1
Max <dev@Open.so>
2020-12-26 13:34:11 +0530

    temp Roles.so v0.1

--- contracts/Roles.sol
+++ contracts/Roles.sol
@@ -1,9 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Roles {
+contract Roles  {
   
-  constructor() {
+  mapping(bytes32 => address[]) private accessRegistry;
+  mapping(address => uint256) private accessCheck;
 
+  address private admin;
+  
+  constructor(address _minter, address _pauser, address _burn, address _admin)  {
+    admin = _admin;
   }
+
+  function addRole(address _member, bytes32 _role)  external onlyAdmin() {
+    
+    require(msg.sender == admin,'Inadequate permissions);
+    accessRegistry[_role].push(_member);
+    accessCheck[_member] == accessCheck.length + 1;
+    
+  }
+
 }
