01379ccf06e3f46fd109d6f4a00ef89463ad771f
Max <max@open.so>
2021-01-12 12:07:03 +0530

    token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,212 +1,204 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0 <0.9.0;
 
-import ".././contracts/util/Context.sol";
-import "contracts/access/AccessControl";
+import "contracts/utils/Context.sol";
+import "contracts/utils/Address.sol";
 
-contract Token is Context{
+contract OSOERC20 is Context {
+    using Address for address;
 
-  AccessControl private access;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
 
-  string  public  _name = "Open protocol";
-  string  public  _symbol = "OSO";
-  uint256 public  _decimals = 6;
+    uint256 public totalSupply;
+    uint256 private cappedSupply;
 
-  uint256 public _totalSupply; 
-  uint256 public  _cappedSupply;
+    address private admin;
 
-  bool public _paused;
-  bool internal _reentrant = false;
+    bool _reentrant;
+    bool _paused;
 
-  
-  address private adminAddress;
-  bytes32 private adminToken;
+    mapping(address => uint256) _balances;
+    mapping(address => mapping(address => uint256)) _allowances;
 
-  address private minterAddress;
-  address private burnerAddress;
-  address private pauserAddress;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 indexed _value,
+        uint256 _timeStamp
+    );
+    event Approved(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 indexed _value,
+        uint256 _timeStamp
+    );
 
-  bytes32 private minter;
-  bytes32 private burner;
-  bytes32 private pauser;
+    event PauseState(address indexed _pauser, bool _paused, uint256 _timeStamp);
 
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_,
+        uint256 initialSupply_,
+        uint256 cappedSupply_,
+        address admin_
+    ) {
+        name = name_;
+        symbol = symbol_;
+        decimals = decimals_;
 
-  mapping(address => uint256) _balances; 
-  mapping(address => mapping(address => uint256)) _allowances;
+        admin = admin_;
+        _balances[admin] = initialSupply_;
+        totalSupply = initialSupply_;
 
-  event Transfer(address indexed _from,address indexed _to,uint256 indexed _value, uint256 _timeStamp);
-  event Approved (address indexed _owner,address indexed _spender,uint256 indexed _value,uint256 _timeStamp);
+        cappedSupply = cappedSupply_;
 
-  event PauseState (address indexed _pauser, _paused, uint256 _timeStamp);
-  event Minted(address indexed _from, address indexed _to, uint256 amount);
-  event Burned(address indexed _from, address _to, uint256 amount);
+        _paused = false;
+        _reentrant = false;
+    }
 
-  constructor(address admin_, address pauser_, address minter_, address burner_, uint256 initialSupply_, uint256 cappedSupply_, AccessControl access_)  {
-    _totalSupply = initialSupply_;
-    _cappedSupply = cappedSupply_;
+    receive() external payable {
+        payable(admin).transfer(_msgValue());
+    }
 
-    _balances[adminAddress] = initialSupply_;
+    fallback() external payable {
+        payable(admin).transfer(_msgValue());
+    }
 
-    adminAddress = admin_;
-    minterAddress = minter_;
-    burnerAddress = burner_;
-    pauserAddress = pauser_;
+    function pauseState() external view returns (string memory) {
+        if (_paused == true) {
+            return "Token transfers are disabled. Contract paused";
+        }
+        return "Contract is active";
+    }
 
-    access._addAdmin(adminToken, adminAddress);
-    access._addRole(minter, minterAddress);
-    access._addRole(burner, burnerAddress);
-    access._addRole(pauser, pauserAddress);
+    function transfer(address _to, uint256 _value)
+        external
+        nonReentrant()
+        returns (bool)
+    {
+        _preTransferCheck();
+        require(_balances[_msgSender()] >= _value, "Insufficient balance");
 
-    _paused = false;
+        _balances[_msgSender()] -= _value;
+        _balances[_to] += _value;
 
-    _totalSupply = initialSupply_;
-    _cappedSupply = cappedSupply_;
+        emit Transfer(_msgSender(), _to, _value, block.timestamp);
 
-    _balances[adminAddress] = initialSupply_;
+        return true;
+    }
 
-    access = access_;
-  }
+    function approve(address _spender, uint256 _value) external returns (bool) {
+        _preTransferCheck();
+        _allowances[_msgSender()][_spender] = 0;
 
-  fallback() external payable {
-    adminAddress.transfer(_msgValue());
-  }
+        require(
+            _balances[_msgSender()] >= _value,
+            "Insuffficient balance, or you do not have necessary permissions"
+        );
+        _allowances[_msgSender()][_spender] += _value;
 
-  receive() external payable {
-    adminAddress.transfer(_msgValue());
-  }
+        emit Approved(_msgSender(), _spender, _value, block.timestamp);
+        return true;
+    }
 
-  function balanceOf(address _account) external view returns (uint256)  {
-    return _balances[_account];
-  }
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external nonReentrant() returns (bool) {
+        _preTransferCheck();
+        require(
+            _allowances[_from][_msgSender()] >= _value &&
+                _balances[_from] >= _value,
+            "Insufficient allowances, or balance"
+        );
 
-  function allowances(address _owner,address _spender) external view returns(uint256)  {
-    return _allowances[_owner][_spender];
-  }
+        _balances[_from] -= _value;
+        _balances[_to] += _value;
 
-  function transfer(address _to, uint256 _value)  external nonReentrant() returns(bool){
-    require(_preTransferCheck(), "This contract is temporarily paused. Regret the inconvenience");
-    require(_balances[_msgSender()] >= _value, "Insufficient balance"); 
-    
-    _balances[_msgSender()] -= _value;
-    _balances[_to] += _value;
-    
-    emit Transfer(_msgSender(), _to, _value, block.timestamp);
+        _allowances[_from][_msgSender()] -= _value;
 
-    return true;
-  }
+        emit Transfer(_from, _to, _value, block.timestamp);
 
-  function approve(address _spender, uint256 _value) external returns(bool) {
-    require(_preTransferCheck(), "This contract is temporarily paused. Regret the inconvenience");
-    _allowances[_msgSender()][_spender] = 0;
-    
-    require(_balances[_msgSender()] >= _value , "Insuffficient balance, or you do not have necessary permissions");
-    _allowances[_msgSender()][_spender] +=_value;
+        return true;
+    }
 
-    emit Approved(_msgSender(), _spender, _value, block.timestamp);
-    return true;
-  }
+    function mint(address _to, uint256 amount)
+        external
+        onlyAdmin()
+        nonReentrant()
+        returns (bool)
+    {
+        require(totalSupply <= cappedSupply, "Exceeds capped supply");
+        require(amount != 0 && _to != address(0), "you can not mint 0 tokens");
 
-  function transferFrom(address _from, address _to, uint256 _value) external nonReentrant() returns (bool)  {
-    require(_preTransferCheck(), "This contract is temporarily paused. Regret the inconvenience");
-    require(_allowances[_from][_msgSender()]>=_value && _balances[_from]>= _value, "Insufficient allowances, or balance");
+        _balances[_to] += amount;
+        totalSupply += amount;
 
-    _balances[_from] -= _value;
-    _balances[_to] += _value;
+        return true;
+    }
 
-    _allowances[_from][_msgSender()] -= _value;
+    function burn(address account, uint256 amount)
+        external
+        onlyAdmin()
+        nonReentrant()
+        returns (bool)
+    {
+        require(
+            account != address(0),
+            "You can not burn tokens from this address"
+        );
 
-    emit Transfer(_from, _to, _value, block.timestamp);
+        _balances[account] -= amount;
+        totalSupply -= amount;
 
-    return true;
-  }
+        emit Transfer(account, address(0), amount, block.timestamp);
 
-  function mint(address _to, uint256 amount) external onlyMinter() nonReentrant() returns(bool)   {
-    require(_totalSupply <= _cappedSupply, "Exceeds capped supply");
-    require(amount !=0 & _to != address(0), "you can not mint 0 tokens");
+        return true;
+    }
 
-    _balances[_to] += amount;
-    _totalSupply += amount;
+    function pause() external onlyAdmin() nonReentrant() {
+        _pause();
+    }
 
-    return true;
-  }
+    function unpause() external onlyAdmin() nonReentrant() {
+        _unpause();
+    }
 
-  function burn(address account,uint256 amount) external onlyBurner() nonReentrant() returns(bool)  {
-    require(account !=address(0), "You can not burn tokens from this address");
+    function _preTransferCheck() internal view {
+        require(
+            _paused == false,
+            "The contract is paused. Transfer functions are temporarily disabled"
+        );
+        this;
+    }
 
-    _balances[account] -= amount;
-    _totalSupply -= amount;
-    
-    emit Transfer(account, address(0), amount);
+    function _pause() internal {
+        require(_paused == false, "The contract is already paused");
+        _paused = true;
 
-    return true;
-  }
-  
-  function pause() external onlyPauser() {
-    _pause();
-  }
+        emit PauseState(_msgSender(), true, block.timestamp);
+    }
 
-  function unPause() external onlyPauser()  {
-    _unPause();
-  }
+    function _unpause() internal {
+        require(_paused == true, "The contract is already paused");
+        _paused = false;
 
-  function _pause() internal  {
-    require(_paused == false, 'The contract is already paused');
+        emit PauseState(_msgSender(), false, block.timestamp);
+    }
 
-    _paused = true;
+    modifier nonReentrant() {
+        require(_reentrant == false, "Re-entrant");
+        _reentrant = true;
+        _;
+        _reentrant = false;
+    }
 
-    emit PauseState(_msgSender(), true, block.timestamp);
-  }
-
-  function _unPause() internal  {
-    require(_paused == true, 'The contract is not paused');
-
-    _paused = false;
-    emit PauseState(_msgSender(), false, block.timestamp);
-  }
-
-  function _preTransferCheck() internal view {
-    require(!_paused == true, 'The contract is paused. Token transfers are temporarily disabled');
-    this;
-  }
-
-  modifier nonReentrant() {
-    require(_reentrant == false, "ReentrancyGuard: reentrant call");
-    _reentrant = true;
-    _;
-    _reentrant = false;
-  }
-  function removeRole(bytes32 role, address account) external onlyAdmin(adminToken,adminAddress)  {
-    access._revokeRole(role, account);
-  }
-  function transferRole(bytes32 role, address oldAccount, address newAccount) external  {
-    access.transferRole(role,oldAccount,newAccount) ;
-  }
-
-  function renounceRole(bytes32 role, address oldAccount) external {
-    access.renounceRole(role, account);
-  }
-
-  function hasAdminRole(bytes32 role, address account) private view returns(bool) {
-    require(access.hasAdminRole(role, account), "Does not have an admin role.");
-  }
-
-  modifier onlyAdmin()  {
-    require(access.hasAdminRole(adminToken, adminAddress), "Role does not exist.");
-    _;
-  }
-
-  modifier onlyPauser()  {
-    require(access.hasRole(pauser, pauserAddress) || access.hasAdminRole(adminToken, adminAddress), "Role does not exist.");
-    _;
-  }
-
-  modifier onlyMinter() {
-    require(access.hasRole(minter, minterAddress) || access.hasAdminRole(adminToken, adminAddress), "Role does not exist.");
-    _;
-  }
-
-  modifier onlyBurner() {
-    require(access.hasRole(burner, burnerAddress) || access.hasAdminRole(adminToken, adminAddress), "Role does not exist.");
-    _;
-  }
+    modifier onlyAdmin() {
+        require(_msgSender() == admin, "Inadequate permission");
+        _;
+    }
 }
